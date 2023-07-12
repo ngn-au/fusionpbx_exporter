8e6f15c7e408920e0a6437fd16225736b5505357
Jad Seifeddine <jadseifeddine@Jads-MacBook-Air.local>
2023-07-13 07:59:31 +1000

    changes

--- collector.go
+++ collector.go
@@ -16,9 +16,9 @@ var (
 	})
 	extensionsCount = make(map[string]prometheus.Gauge)
 	answeredCalls   = make(map[string]prometheus.Gauge)
+	missedCalls   = make(map[string]prometheus.Gauge)
 	outboundCalls = make(map[string]prometheus.Gauge)
 	inboundCalls = make(map[string]prometheus.Gauge)
-
 	mosMetrics = make(map[string]prometheus.Gauge)
 	durationMetrics = make(map[string]prometheus.Gauge)
 )
@@ -33,6 +33,9 @@ func CollectMetrics() {
 		g.Set(0)
 	}
 	for _, g := range answeredCalls {
+		g.Set(0)
+	}
+	for _, g := range missedCalls {
 		g.Set(0)
 	}
 	for _, g := range outboundCalls {
@@ -115,6 +118,32 @@ func CollectMetrics() {
 		gauge.Set(answeredCallsCount.Float64)
 	}
 
+	rows, err = db.Query("SELECT d.domain_name, COUNT(*) FROM v_domains d JOIN v_xml_cdr c ON d.domain_uuid = c.domain_uuid WHERE c.hangup_cause = 'ORIGINATOR_CANCEL' AND c.end_stamp > clock_timestamp() - INTERVAL '30 seconds' GROUP BY d.domain_name")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var domain string
+		var missedCallsCount sql.NullFloat64
+		if err := rows.Scan(&domain, &missedCallsCount); err != nil {
+			log.Fatal(err)
+		}
+
+		gauge, ok := missedCalls[domain]
+		if !ok {
+			gauge = prometheus.NewGauge(prometheus.GaugeOpts{
+				Name:        "missed_calls_per_domain",
+				Help:        "Number of missed calls per domain",
+				ConstLabels: prometheus.Labels{"domain": domain},
+			})
+			reg.MustRegister(gauge)
+			missedCalls[domain] = gauge
+		}
+
+		gauge.Set(missedCallsCount.Float64)
+	}
+
 	rows, err = db.Query("SELECT domain_name, COUNT(*) FROM v_xml_cdr WHERE direction = 'outbound' AND end_stamp > clock_timestamp() - INTERVAL '30 seconds' GROUP BY domain_name")
 	if err != nil {
 		log.Fatal(err)
@@ -182,7 +211,7 @@ func CollectMetrics() {
 		gauge, ok := mosMetrics[domain]
 		if !ok {
 			gauge = prometheus.NewGauge(prometheus.GaugeOpts{
-				Name:        "average_mos_per_domain",
+				Name:        "avg_mos_per_domain",
 				Help:        "Average MOS per domain",
 				ConstLabels: prometheus.Labels{"domain": domain},
 			})
@@ -209,7 +238,7 @@ func CollectMetrics() {
 		if !ok {
 			gauge = prometheus.NewGauge(prometheus.GaugeOpts{
 				Name:        "avg_call_duration_per_domain",
-				Help:        "Average call duration per domain in the last 15s",
+				Help:        "Average call duration per domain",
 				ConstLabels: prometheus.Labels{"domain": domain},
 			})
 			reg.MustRegister(gauge)
